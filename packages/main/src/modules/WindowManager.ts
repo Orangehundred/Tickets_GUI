ad4a217d51461128545b566e51f36ce4e66b3eab
Orangehundred <victor118877@gmail.com>
2026-03-04 17:06:51 -0600

    Window Size change & Ticket Example
    Window Size change & Ticket Example that doesn't work. Just foundations for a playwright test.

--- packages/main/src/modules/WindowManager.ts
+++ packages/main/src/modules/WindowManager.ts
@@ -24,6 +24,8 @@ class WindowManager implements AppModule {
   async createWindow(): Promise<BrowserWindow> {
     const browserWindow = new BrowserWindow({
       show: false, // Use the 'ready-to-show' event to show the instantiated BrowserWindow.
+      width: 1200,
+      height: 800,
       webPreferences: {
         nodeIntegration: false,
         contextIsolation: true,
